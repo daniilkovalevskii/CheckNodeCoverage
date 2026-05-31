b561191bef735623128e2ca4065b0707bee7863a
Daniil <kovalevskiydaniil@gmail.com>
2026-05-31 12:22:43 +0300

    Added Node and Error class methods realization

--- structures.h
+++ structures.h
@@ -14,20 +14,37 @@ enum NodeShape
 class Node
 {
 public:
-    Node();
-    Node(QString newName, NodeShape newShape);
+    Node(QString newName, NodeShape newShape = NodeShape::DEFAULT)
+    {
+        name = newName;
+        shape = newShape;
+
+        parent = nullptr;
+    };
+
+    ~Node()
+    {
+        for (Node* child : children)
+        {
+            delete child;
+        }
+    }
 
     QString name;
     NodeShape shape;
     Node* parent;
     QVector<Node*> children;
 
-    bool isLeaf() const;
-    bool operator== (Node &other);
+    bool isLeaf() const
+    {
+        return children.isEmpty();
+    };
 };
 
-enum ErrorType {
+enum ErrorType
+{
     FILE_ERROR,
+    OUTPUT_FILE_ERROR,
     CONNECTIVITY_ERROR,
     NO_MARKED_NODES,
     LEAF_TARGET,
@@ -44,18 +61,91 @@ enum ErrorType {
 
 class Error
 {
-    Error();
-    Error(ErrorType type, QString nodeName1, QString nodeName2, QString path, int line);
+    public:
+        Error()
+        {
+        };
+        Error(ErrorType newType, QString nodeName1 = "", QString nodeName2 = "", QString newPath = "", int newLine = -1)
+        {
+            type = newType;
+            nodeName = nodeName1;
+            relatedNodeName = nodeName2;
+            path = newPath;
+            line = newLine;
+        };
 
-    ErrorType type;
-    QString nodeName;
-    QString relatedNodeName;
-    QString path;
-    int line;
+        ErrorType type;
+        QString nodeName;
+        QString relatedNodeName;
+        QString path;
+        int line;
 
-    QString generateMessage() const;
-    bool operator==(const Error& other) const;
+        QString generateMessage() const
+        {
+            switch (type)
+            {
+                case FILE_ERROR:
+                    return "Входной файл не найден или недоступен для чтения.";
+
+                case OUTPUT_FILE_ERROR:
+                    return "Неверно указан файл для выходных данных. Возможно указанного расположения не существует или нет прав на запись.";
+
+                case CYCLE_ERROR:
+                    return QString("Обнаружен цикл %1. Граф не является деревом.").arg(path);
+
+                case FORBIDDEN_STRUCTURE_OR_FORM:
+                    return QString("Обнаружена запрещенная структура или сложный тип узла в строке %1. Используйте только плоский digraph и простые формы.").arg(line);
+
+                case NO_TARGET:
+                    return "Не найдено целевого узла.";
+
+                case CONNECTIVITY_ERROR:
+                    return QString("Нарушена связность. Нет связи между узлами %1 и %2.").arg(nodeName, relatedNodeName);
+
+                case SYNTAX_ERROR:
+                    return QString("Синтаксическая ошибка в описании графа DOT в строке %1.").arg(line);
+
+                case MULTIPLE_TARGETS:
+                    return QString("Обнаружено более одного целевого узла (Узлы %1). Допустим только один целевой узел.").arg(relatedNodeName);
+
+                case NOT_DESCENDANT:
+                    return QString("Узел %1 не является потомком целевого узла.").arg(nodeName);
+
+                case REDUNDANT_NODE:
+                    return QString("Узел %1 избыточен (находится в поддереве уже отмеченного узла %2).").arg(nodeName, relatedNodeName);
+
+                case LEAF_TARGET:
+                    return QString("Целевой узел %1 не может быть листом.").arg(nodeName);
+
+                case MULTI_PARENT:
+                    return QString("Узел %1 имеет несколько родителей.").arg(nodeName);
+
+                case NO_ROOT:
+                    return "Не удалось определить корень дерева.";
+
+                case NO_MARKED_NODES:
+                    return "Нет отмеченных узлов покрытия.";
+            }
+
+            return "";
+        }
+        bool operator==(const Error& other) const
+        {
+            return type == other.type &&
+                   nodeName == other.nodeName &&
+                   relatedNodeName == other.relatedNodeName &&
+                   path == other.path &&
+                   line == other.line;
+        };
 };
+
+inline size_t qHash(const Error& key, size_t seed = 0)
+{
+    return qHash(static_cast<int>(key.type), seed) ^
+           qHash(key.nodeName, seed) ^
+           qHash(key.path, seed) ^
+           qHash(key.line, seed);
+}
 
 enum CoverageStatus
 {
