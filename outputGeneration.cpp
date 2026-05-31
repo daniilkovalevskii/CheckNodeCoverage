c25736d819686df387e12b071dfce0cd7477238a
Daniil <kovalevskiydaniil@gmail.com>
2026-05-31 13:24:00 +0300

    Added headers and stubs for generateReport, generateOutputDOT

---
+++ outputGeneration.cpp
@@ -0,0 +1,11 @@
+#include "outputGeneration.h"
+
+bool generateReport(const QString& outPath, const QSet<Error>& errors, const Result& result)
+{
+    return false;
+}
+
+bool generateOutputDOT(const QString& outPath, const QSet<Error>& errors, QStringList& lines, const Result& result)
+{
+    return false;
+}
---
+++ outputGeneration.h
@@ -0,0 +1,12 @@
+#ifndef OUTPUTGENERATION_H
+#define OUTPUTGENERATION_H
+#include "structures.h"
+
+
+// Функция для создания файла с отчетом
+bool generateReport(const QString& outPath, const QSet<Error>& errors, const Result& result);
+
+// Функция для создания выходного файла .dot
+bool generateOutputDOT(const QString& outPath, const QSet<Error>& errors, QStringList& lines, const Result& result);
+
+#endif // OUTPUTGENERATION_H
