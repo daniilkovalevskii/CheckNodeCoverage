eadcdf43dc473f29d2403a617b2609a2bf21b678
Daniil <kovalevskiydaniil@gmail.com>
2026-06-09 12:49:50 +0300

    Added function readDotFile to testHelpers.h

--- tests/testHelpers.h
+++ tests/testHelpers.h
@@ -6,5 +6,26 @@
 #include <QDebug>
 #include "QTest"
 
+// Функция для чтения строк из файла
+QStringList readDotFile(const QString& relativePath)
+{
+    QString fullPath = QFINDTESTDATA(relativePath);
+    if (fullPath.isEmpty()) {
+        fullPath = QCoreApplication::applicationDirPath() + "/" + relativePath;
+    }
+
+    QFile file(fullPath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qWarning() << "Не удалось открыть тестовый файл:" << fullPath;
+        return QStringList();
+    }
+
+    QTextStream in(&file);
+    QStringList lines;
+    while (!in.atEnd()) {
+        lines.append(in.readLine());
+    }
+    return lines;
+}
 
 #endif // TESTHELPERS_H
