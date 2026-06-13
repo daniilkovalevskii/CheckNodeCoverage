abd80f8e9185fa2c562b14b2599ccb2865bcd423
Daniil <kovalevskiydaniil@gmail.com>
2026-06-13 13:41:25 +0300

    Added dfsCovered function realization

--- CheckNodeCoverage/coverageAnalysis.cpp
+++ CheckNodeCoverage/coverageAnalysis.cpp
@@ -1,4 +1,6 @@
 #include "coverageAnalysis.h"
+#include <QSet>
+#include <QDebug>
 
 void dfsAbove(Node* node, QSet<Error>& errors, Result& result)
 {
@@ -12,5 +14,16 @@ CoverageStatus dfsSearching(Node* node, QSet<Error>& errors, Result& result)
 
 void dfsCovered(Node* node, Node* activeMarked, QSet<Error>& errors)
 {
-    return;
+    // Для каждого ребенка текущего узла
+    for (Node* child : node->children)
+    {
+        // Если ребенок отмечен(имеет форму rectangle), добавляем ошибку
+        if (child->shape == NodeShape::RECTANGLE)
+        {
+            errors.insert(Error(ErrorType::REDUNDANT_NODE, QString("%1").arg(child->name), QString("%1").arg(activeMarked->name)));
+        }
+
+        // Запускаем рекурсию от ребенка, сохранив указатель на уже отмеченный узел
+        dfsCovered(child, activeMarked, errors);
+    }
 }
