c149c0e34dbd48cc7a78b6668d4605d785eaad4a
Daniil <kovalevskiydaniil@gmail.com>
2026-06-14 10:47:03 +0300

    Added documentation to coverageAnalysis.h

--- CheckNodeCoverage/coverageAnalysis.h
+++ CheckNodeCoverage/coverageAnalysis.h
@@ -2,14 +2,37 @@
 #define COVERAGEANALYSIS_H
 #include "structures.h"
 
+/*!
+ * \file coverageAnalysis.h
+ * \brief Функции для проверки покрытия целевого узла набором отмеченных узлов
+ */
 
-// Функция для обработки узлов выше целевого
+/*!
+ * \brief Рекурсивно обрабатывает узлы выше целевого.
+ *
+ * Ищет ошибочно отмеченные узлы вне поддерева целевого узла и ищет сам целевой узел.
+ *
+ * \param[in] node Текущий анализируемый узел
+ * \param[in,out] errors Набор ошибок
+ * \param[in,out] result Структура с результатами анализа покрытия целевого узла
+ */
 void dfsAbove(Node* node, QSet<Error>& errors, Result& result);
 
-// Функция для обработки узлов между целевым и листьями
+/*!
+ * \brief Рекурсивно проверяет покрытие целевого узла набором отмеченных узлов.
+ * \param[in] node Текущий анализируемый узел
+ * \param[in,out] errors Набор ошибок
+ * \param[in,out] result Структура с результатами анализа покрытия целевого узла
+ * \return Статус покрытия для узла
+ */
 CoverageStatus dfsSearching(Node* node, QSet<Error>& errors, Result& result);
 
-// Функция для обработки узлов ниже уже отмеченных
+/*!
+ * \brief Рекурсивно ищет ошибочно отмеченные узлы (избыточные) ниже целевого.
+ * \param[in] node Указатель на текущий узел
+ * \param[in] activeMarked Указатель на отмеченный узел, поддерево которого анализируется
+ * \param[in,out] errors Набор ошибок
+ */
 void dfsCovered(Node* node, Node* activeMarked, QSet<Error>& errors);
 
 #endif // COVERAGEANALYSIS_H
